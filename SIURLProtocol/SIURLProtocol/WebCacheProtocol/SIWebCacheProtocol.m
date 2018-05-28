b0d51599f97c735b3260339e7de24b161fdc2c13
Silence <374619540@qq.com>
2018-05-28 22:06:00 +0800

    Update

--- SIURLProtocol/SIURLProtocol/WebCacheProtocol/SIWebCacheProtocol.m
+++ SIURLProtocol/SIURLProtocol/WebCacheProtocol/SIWebCacheProtocol.m
@@ -30,6 +30,18 @@
 @end
 
 static NSString *const SIWebCacheProtocolIdentifier = @"SIWebCacheProtocolIdentifier";
+
+@interface SIWebCacheProtocol()<NSURLSessionDataDelegate>
+
+@property (nonatomic, strong) NSURLSessionDataTask *dataTask;
+@property (nonatomic, strong) NSOperationQueue     *sessionDelegateQueue;
+
+@property (nonatomic, readwrite, strong) NSMutableData *data;
+@property (nonatomic, readwrite, strong) NSURLResponse *response;
+
+@end
+
+
 @implementation SIWebCacheProtocol
 
 + (BOOL)canInitWithRequest:(NSURLRequest *)request {
@@ -54,14 +66,96 @@ static NSString *const SIWebCacheProtocolIdentifier = @"SIWebCacheProtocolIdenti
     BOOL reachable = [[AFNetworkReachabilityManager managerForDomain:self.request.URL.host] isReachable]
     ;
     return !reachable;
-    
 }
 
-- (NSString *)cachePathForRequest:(NSURLRequest *)aRequest {
+- (NSString *)cachePathForRequest:(NSURLRequest *)aRequest{
     NSString *cachesPath = [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) lastObject];
-    NSString *fileName = [aRequest.URL.absoluteString md5String];
-    return [cachesPath stringByAppendingString:fileName];
+    NSString *fileName = [[[aRequest URL] absoluteString] md5String];
+    return [cachesPath stringByAppendingPathComponent:fileName];
 }
+
+- (void)startLoading {
+    if ([self useCache]) {
+        SICachedData *cache = [NSKeyedUnarchiver unarchiveObjectWithFile:[self cachePathForRequest:[self request]]];
+        if (cache) {
+            NSData *data = [cache data];
+            NSURLResponse *response = [cache response];
+            NSURLRequest *redirectRequest = [cache redirectRequest];
+            if (redirectRequest) {
+                [[self client] URLProtocol:self wasRedirectedToRequest:redirectRequest redirectResponse:response];
+            } else {
+                [[self client] URLProtocol:self didReceiveResponse:response cacheStoragePolicy:NSURLCacheStorageNotAllowed];
+                [[self client] URLProtocol:self didLoadData:data];
+                [[self client] URLProtocolDidFinishLoading:self];
+            }
+            return;
+        }
+    }
+    
+    NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
+    self.sessionDelegateQueue = [[NSOperationQueue alloc]init];
+    self.sessionDelegateQueue.maxConcurrentOperationCount = 1;
+    self.sessionDelegateQueue.name = @"com.silence.queue";
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:config delegate:self delegateQueue:self.sessionDelegateQueue];
+    self.dataTask = [session dataTaskWithRequest:self.request];
+    [self.dataTask resume];
+}
+
+- (void)stopLoading {
+    [self.dataTask cancel];
+    
+    [[self client] URLProtocolDidFinishLoading:self];
+    
+    NSString *cachePath = [self cachePathForRequest:[self request]];
+    SICachedData *cache = [SICachedData new];
+    [cache setResponse:[self response]];
+    [cache setData:[self data]];
+    [NSKeyedArchiver archiveRootObject:cache toFile:cachePath];
+}
+
+- (void)appendData:(NSData *)newData{
+    if ([self data] == nil) {
+        [self setData:[newData mutableCopy]];
+    }else {
+        [[self data] appendData:newData];
+    }
+}
+
+#pragma mark - NSURLSessionTaskDelegate
+- (void)URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task didCompleteWithError:(NSError *)error {
+    if (!error) {
+        [self.client URLProtocolDidFinishLoading:self];
+    } else if ([error.domain isEqualToString:NSURLErrorDomain] && error.code == NSURLErrorCancelled) {
+        
+    } else {
+        [self.client URLProtocol:self didFailWithError:error];
+    }
+    self.dataTask = nil;
+}
+
+#pragma mark - NSURLSessionDataDelegate
+- (void)URLSession:(NSURLSession *)session dataTask:(NSURLSessionDataTask *)dataTask
+    didReceiveData:(NSData *)data {
+    [[self client] URLProtocol:self didLoadData:data];
+    [self appendData:data];
+}
+
+- (void)URLSession:(NSURLSession *)session dataTask:(NSURLSessionDataTask *)dataTask didReceiveResponse:(NSURLResponse *)response completionHandler:(void (^)(NSURLSessionResponseDisposition))completionHandler {
+    [self setResponse:response];
+    [[self client] URLProtocol:self didReceiveResponse:response cacheStoragePolicy:NSURLCacheStorageNotAllowed];
+}
+
+- (void)URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task willPerformHTTPRedirection:(NSHTTPURLResponse *)response newRequest:(NSURLRequest *)request completionHandler:(void (^)(NSURLRequest * _Nullable))completionHandler {
+    if (response != nil){
+        NSString *cachePath = [self cachePathForRequest:[self request]];
+        SICachedData *cache = [SICachedData new];
+        [cache setResponse:response];
+        [cache setRedirectRequest:request];
+        [NSKeyedArchiver archiveRootObject:cache toFile:cachePath];
+        [[self client] URLProtocol:self wasRedirectedToRequest:request redirectResponse:response];
+    }
+}
+
 
 @end
 
